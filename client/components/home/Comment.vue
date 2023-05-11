6402971b4e3e7b83e14f9fb6bb0d4503e38c975c
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-11 10:33:48 +0200

    Modification de la carte geste + Ajout de la vue Comment ? ✨

---
+++ client/components/home/Comment.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const steps = [
+  {
+    title: '1.',
+    description: 'Entrez votre adresse afin que nous puissions récupérer les données de votre territoire'
+  },
+  {
+    title: '2.',
+    description: 'Accédez aux restrictions qui vous concernent et aux bons gestes à adopter'
+  }
+]
+</script>
+
+<template>
+  <div class="comment fr-grid-row fr-grid-row--center fr-mt-8w">
+    <div class="comment-title fr-mb-8w">
+      <h1>Comment ça <span class="keyword-highlight">marche</span> ?</h1>
+    </div>
+    <div class="fr-grid-row fr-grid-row--gutters">
+      <div class="fr-col-12 fr-col-md-6 fr-grid-row"
+           v-for="step in steps">
+        <DsfrCard
+          altImg="icone"
+          noArrow
+          link=""
+          detail=""
+          :title="step.title"
+          :description="step.description"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.comment {
+  &-title {
+    text-align: center;
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
