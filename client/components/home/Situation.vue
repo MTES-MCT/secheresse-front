b34d5c7c2a5cefe456f0c7acabe3ed337ca7a604
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-12 10:27:38 +0200

    Mise à jour des tests e2e, rajout check footer & restrictions ✅

--- client/components/home/Situation.vue
+++ client/components/home/Situation.vue
@@ -6,7 +6,7 @@ import situations from '../../data/situations.json'
   <div class="situation fr-grid-row fr-grid-row--center fr-mt-4w fr-mb-4w">
     <div class="section-title fr-mb-8w">
       <h1>La situation en temps réel</h1>
-      <span>Les départements signalés comme en “Alerte”, “Alerte renforcée et “Crise” sont ceux qui subissent actuellement des pénuries d’eau. Ils sont soumis à des mesures à respecter.</span>
+      <span>Les départements signalés comme en “Alerte”, “Alerte renforcée“ et “Crise” sont ceux qui subissent actuellement des pénuries d’eau. Ils sont soumis à des mesures à respecter.</span>
     </div>
     <div class="fr-grid-row fr-grid-row--gutters">
       <div class="fr-col-12 fr-col-md-6 fr-col-lg-3"
