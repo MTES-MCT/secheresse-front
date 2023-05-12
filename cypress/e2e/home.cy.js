4c4a48d19984d7461c72135169ac26b8c69628a1
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-12 10:27:38 +0200

    Mise à jour des tests e2e, rajout check footer & restrictions ✅

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,12 +2,12 @@ describe('Test de la home page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  
+
   it(`Nous devons bien arriver sur la page d'accueil`, () => {
     cy.visit('/');
     cy.location('pathname').should('equal', '/');
   });
-  
+
   it(`La page doit s'afficher correctement`, () => {
     cy.get('body .fr-header').should('exist');
     cy.get('body .presentation').should('exist');
@@ -22,7 +22,6 @@ describe('Test de la home page', () => {
       cy.get('body .fr-header').should('exist');
       cy.get('body .fr-header .fr-logo').contains('Ministères').should('exist');
       cy.get('body .fr-header .fr-logo').contains('Transition écologique').should('exist');
-      cy.get('body .fr-header .fr-header__service').contains('Alerte sécheresse').should('exist');      
     });
   });
 
@@ -34,11 +33,30 @@ describe('Test de la home page', () => {
       cy.get('body .fr-footer .fr-footer__content-list').find('li').should('have.length', 4);
       cy.get('body .fr-footer .fr-footer__bottom-list').find('li').should('have.length', 4);
     });
+    
+    it(`On doit pouvoir accéder aux pages légales depuis le footer`, () => {
+      cy.get('body .fr-footer [data-testid="/a11y"]').should('exist');
+      cy.get('body .fr-footer [data-testid="/mentions-legales"]').should('exist');
+      cy.get('body .fr-footer [data-testid="/donnees-personnelles"]').should('exist');
+      cy.get('body .fr-footer [data-testid="/cookies"]').should('exist');
+
+      cy.get('body .fr-footer [data-testid="/a11y"]').click();
+      cy.location('pathname').should('equal', '/a11y');
+
+      cy.get('body .fr-footer [data-testid="/mentions-legales"]').click();
+      cy.location('pathname').should('equal', '/mentions-legales');
+
+      cy.get('body .fr-footer [data-testid="/donnees-personnelles"]').click();
+      cy.location('pathname').should('equal', '/donnees-personnelles');
+
+      cy.get('body .fr-footer [data-testid="/cookies"]').click();
+      cy.location('pathname').should('equal', '/cookies');
+    });
   });
   
   describe(`Bloc présentation`, () => {
     it(`Le bloc présentation doit s'afficher correctement`, () => {
-      cy.get('body .presentation h1').contains('Restez informé en temps réel des restrictions d\'eau en France').should('exist');
+      cy.get('body .presentation h1').contains('Chaque geste compte, préservons nos ressources').should('exist');
       cy.get('body .presentation [data-cy=AddressSearchInput]').should('exist');
     });
   });
