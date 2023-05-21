36a90b5b54d523fae0f28f29778592910d779b8b
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-21 11:31:16 +0200

    Modification du script Matomo stat.data.gouv 📈

--- client/public/matomo.js
+++ client/public/matomo.js
@@ -4,12 +4,12 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-  var u = "https://secheresse.matomo.cloud/";
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', '1']);
+  var u = "https://stats.data.gouv.fr/";
+  _paq.push(['setTrackerUrl', u + 'piwik.php']);
+  _paq.push(['setSiteId', '285']);
   var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
   g.async = true;
-  g.src = '//cdn.matomo.cloud/secheresse.matomo.cloud/matomo.js';
+  g.src = u + 'piwik.js';
   s.parentNode.insertBefore(g, s);
 })();
-// End Matomo code
+// End Matomo Code
