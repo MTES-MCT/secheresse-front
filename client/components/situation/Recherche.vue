92fbc35293e24b4e6b42d78320c6f321a88703d4
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-29 18:13:49 +0200

    Bug fermeture modale 🐛

--- client/components/situation/Recherche.vue
+++ client/components/situation/Recherche.vue
@@ -21,6 +21,10 @@ const searchRestriction = ($event) => {
   }
   utils.searchRestriction($event, modalTitle, modalText, modalOpened, router);
 }
+
+const closeModal = () => {
+  modalOpened.value = false;
+}
 </script>
 
 <template>
@@ -50,8 +54,8 @@ const searchRestriction = ($event) => {
     <DsfrModal :opened="modalOpened"
                :title="modalTitle"
                icon="ri-arrow-right-line"
-               :actions='[{"label":"Fermer"}]'
-               @close="modalOpened = false">
+               :actions='[{label:"Fermer", onClick: closeModal}]'
+               @close="closeModal">
       {{ modalText }}
     </DsfrModal>
   </div>
