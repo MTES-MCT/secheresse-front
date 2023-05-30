662f93997f6310ca96d6fcfa79a2bb9cca0fa077
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-30 11:36:00 +0200

    Ajout de la date de l'arrêté 💬

--- client/components/situation/status/Header.vue
+++ client/components/situation/status/Header.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import utils from "../../../utils";
 import { Restriction } from "../../../dto/restriction.dto";
+import { Ref } from "vue";
 
 const props = defineProps<{
   restriction: Restriction
   address: any
-}>()
+}>();
 
-const links: any[] = ref([{to: '/', text: 'Accueil'}, {
+const links: Ref<any[]> = ref([{to: '/', text: 'Accueil'}, {
   to: '/situation/recherche',
   text: `Quelle est la situation sur mon territoire ?`
-}, {text: 'Votre adresse'}])
+}, {text: 'Votre adresse'}]);
 
 const badgeLabel = computed<string>(() => {
   return utils.getSituationBadgeLabel(utils.getRestrictionRank(props.restriction));
-})
+});
 
 const situationLabel = computed<string>(() => {
   return utils.getSituationLabel(utils.getRestrictionRank(props.restriction))
-})
+});
+
+const dateArrete = computed<string | null>(() => {
+  if (!props.restriction.arrete?.dateDebutValidite) {
+    return null;
+  }
+  return new Date(props.restriction.arrete.dateDebutValidite).toLocaleDateString('fr-FR');
+});
 
 </script>
 
@@ -38,7 +46,10 @@ const situationLabel = computed<string>(() => {
         <span class="fr-icon-map-pin-user-line fr-mr-1w" aria-hidden="true"></span>
         {{ address?.properties.label }}
       </div>
-      <h3>Votre territoire est actuellement <span :class="'situation-level-c-' + utils.getRestrictionRank(restriction)">{{ situationLabel }}</span></h3>
+      <h3>Votre territoire est actuellement <span :class="'situation-level-c-' + utils.getRestrictionRank(restriction)">{{
+          situationLabel
+        }}</span></h3>
+      <div v-if="dateArrete">Arrêté en date du {{ dateArrete }}</div>
       <div>Le respect des restrictions <b>est obligatoire</b> sous peine de recevoir une <b>amende</b> de 1 000 000€</div>
     </div>
     <div class="fr-col-12 fr-col-md-6 button-wrapper">
