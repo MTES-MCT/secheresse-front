d81f8ffcccd8d2922cf8387ccf18c91fe263e751
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-05-30 11:57:30 +0200

    Wording commune + thématiques 💬

--- client/pages/accessibilite/index.vue
+++ client/pages/accessibilite/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Ref } from "vue";
+
 definePageMeta({
   layout: 'basic',
 })
@@ -7,7 +9,7 @@ useHead({
   title: `Préservons l'eau - Accessibilité`
 })
 
-const links: any[] = ref([{"to": "/", "text": "Accueil"}, {"text": "Accessibilité"}])
+const links: Ref<any[]> = ref([{"to": "/", "text": "Accueil"}, {"text": "Accessibilité"}])
 </script>
 
 <template>
