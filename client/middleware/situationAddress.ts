d111da3dd223fea63add2331e0c8e7f2cfc11a7d
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-08 09:38:51 +0200

    Ajout de query params dans l'URL pour pouvoir la partager plus facilement ✨

--- client/middleware/situationAddress.ts
+++ client/middleware/situationAddress.ts
@@ -2,13 +2,13 @@ import { useAddressStore } from "../store/address";
 import { storeToRefs } from "pinia";
 import { useRestrictionsStore } from "../store/restrictions";
 
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to: any, from: any) => {
   const addressStore = useAddressStore();
   const {address, geo} = storeToRefs(addressStore);
   const restrictionsStore = useRestrictionsStore();
   const {restrictions} = storeToRefs(restrictionsStore);
-  
+
   if ((!address.value && !geo.value) || !restrictions.value) {
-    return navigateTo('/')
+    return navigateTo({path: '/', query: to.query})
   }
 })
