58abb22404ae5bc29b7e1b9608948cc02cc72adf
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-09 08:52:39 +0200

    Taille du bundle ⚡️

--- client/plugins/vue-dsfr.ts
+++ client/plugins/vue-dsfr.ts
@@ -1,13 +1,49 @@
-import VueDsfr from '@gouvminint/vue-dsfr'
+import {
+  DsfrFooter,
+  DsfrHeader,
+  DsfrBreadcrumb,
+  DsfrSearchBar,
+  DsfrAccordionsGroup,
+  DsfrAccordion,
+  DsfrCard,
+  DsfrTag,
+  DsfrTabs,
+  DsfrTabContent,
+  DsfrNotice,
+  DsfrModal,
+  DsfrBadge,
+  DsfrStepper,
+  DsfrCallout
+} from '@gouvminint/vue-dsfr'
 import { EauMaison, EauSoleil, EauSouterraine, EauSurface } from "../assets/icons";
+import { addIcons, OhVueIcon } from 'oh-vue-icons';
+import { RiArrowRightLine } from 'oh-vue-icons/icons/ri/index.js';
 
 const icons = [
   EauMaison,
   EauSoleil,
   EauSouterraine,
-  EauSurface
+  EauSurface,
+  RiArrowRightLine
 ]
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueDsfr, {icons})
+  // nuxtApp.vueApp.use(VueDsfr, {icons})
+  nuxtApp.vueApp.component('DsfrHeader', DsfrHeader);
+  nuxtApp.vueApp.component('DsfrFooter', DsfrFooter);
+  nuxtApp.vueApp.component('DsfrBreadcrumb', DsfrBreadcrumb);
+  nuxtApp.vueApp.component('DsfrSearchBar', DsfrSearchBar);
+  nuxtApp.vueApp.component('DsfrAccordionsGroup', DsfrAccordionsGroup);
+  nuxtApp.vueApp.component('DsfrAccordion', DsfrAccordion);
+  nuxtApp.vueApp.component('DsfrCard', DsfrCard);
+  nuxtApp.vueApp.component('DsfrTag', DsfrTag);
+  nuxtApp.vueApp.component('DsfrTabs', DsfrTabs);
+  nuxtApp.vueApp.component('DsfrTabContent', DsfrTabContent);
+  nuxtApp.vueApp.component('DsfrNotice', DsfrNotice);
+  nuxtApp.vueApp.component('DsfrModal', DsfrModal);
+  nuxtApp.vueApp.component('DsfrBadge', DsfrBadge);
+  nuxtApp.vueApp.component('DsfrStepper', DsfrStepper);
+  nuxtApp.vueApp.component('DsfrCallout', DsfrCallout);
+  addIcons(...icons);
+  nuxtApp.vueApp.component('VIcon', OhVueIcon);
 })
