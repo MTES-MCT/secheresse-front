c4e36af3cb0099497ed62ca33c942560f92d562f
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-12 14:15:01 +0200

    Ajout d'icones 💄

--- client/components/gestes/Card.vue
+++ client/components/gestes/Card.vue
@@ -8,12 +8,15 @@ const props = defineProps<{
   <div class="fr-col-12 fr-col-md-4" :class="geste.large ? '' : 'fr-col-lg-3'">
     <div class="eau-card fr-p-2w">
       <h6 class="eau-card__title fr-my-2w">
-        <VIcon v-if="geste.icon"
-               :name="geste.icon"
-               scale="3"/>
-        <br v-if="geste.icon"/>
-        <br v-if="geste.icon"/>
-        {{ geste.title }}
+        <div class="fr-grid-row">
+          <div v-if="geste.icon" class="fr-col-3" :style="'max-width: 60px'">
+            <VIcon :name="geste.icon"
+                   scale="3"/>
+          </div>
+          <div :class="geste.icon ? 'fr-col-9' : 'fr-col-12'">
+            {{ geste.title }}
+          </div>
+        </div>
       </h6>
       <div class="eau-card__desc" v-html="geste.description">
       </div>
