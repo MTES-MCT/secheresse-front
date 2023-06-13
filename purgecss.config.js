467f243ac47f7b56dff6ec6d92f4eace6979e9d8
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-13 17:19:09 +0200

    Bug css 🐛

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['.output/public/**/*.html', '.output/public/**/*.js'],
   css: ['.output/public/**/*.css'],
-  safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^situation-level-/],
+  safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^situation-level-/, /^fr-badge--/],
   output: ['.output/public/_nuxt']
 }
