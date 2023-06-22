c918834803ae5f5353e43e70928cd6861ce08ac6
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-22 09:58:38 +0200

    Ajout des répartitions d'usages de l'eau ✨

--- client/components/accueil/Gestes.vue
+++ client/components/accueil/Gestes.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import gestes from '../../data/gestes.json'
+import repartition from '../../data/repartition.json'
 import { Ref } from "vue";
 import { TagProps } from "@gouvminint/vue-dsfr/types/components/DsfrTag/DsfrTag.vue";
 
@@ -24,6 +25,12 @@ const selectedTagIndex: Ref<number> = ref(0);
   <div class="gestes fr-grid-row fr-grid-row--center fr-pt-8w fr-pb-8w">
     <div class="section-title fr-mb-4w">
       <h1>Nous consommons 148 litres d’eau potable par jour et par personne !</h1>
+      <div class="fr-grid-row fr-grid-row--center fr-mb-1w">
+        <GestesRepartition v-for="r in repartition.data"
+                           :percent="r.percent" 
+                           :icon="r.icon" 
+                           :description="r.description" />
+      </div>
       <span>En plus des restrictions, l’adoption des éco-gestes est un bon moyen de préserver les ressources en eau et d’éviter que la situation s’aggrave. Voici quelques exemples d’habitudes à prendre pour limiter sa consommation d’eau à l’échelle individuelle.</span>
     </div>
     <div class="fr-grid-row fr-grid-row--gutters">
---
+++ client/components/gestes/Repartition.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  percent: number,
+  icon: string,
+  description: string
+}>();
+</script>
+
+<template>
+  <div class="repartition">
+    <div class="small c100 center" :class="'p' + percent">
+      <span><VIcon :name="icon" scale="2.5"/></span>
+      <div class="slice">
+        <div class="bar"></div>
+        <div class="fill"></div>
+      </div>
+    </div>
+    <div class="repartition--desc">
+      <b>{{ percent }}%</b><br/>
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.repartition {
+  width: 100px;
+  font-size: 0.9rem;
+  line-height: 1.2rem;
+}
+</style>
