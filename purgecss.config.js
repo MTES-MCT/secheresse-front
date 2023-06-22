afd7591fe4183f152a2828a46aac8d5e60dd0b65
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-06-22 11:32:12 +0200

    Bug purgecss 🐛

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['.output/public/**/*.html', '.output/public/**/*.js'],
   css: ['.output/public/**/*.css'],
-  safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^situation-level-/, /^fr-p/, /^fr-col/],
+  safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^situation-level-/, /^fr-p/, /^fr-col/, /^p/],
   output: ['.output/public/_nuxt']
 }
