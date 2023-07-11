69ad79bcfca57d44eb170c03b4bae64edc3334ea
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-07-11 13:54:01 +0200

    Wording 💬

--- client/components/accueil/Carte.vue
+++ client/components/accueil/Carte.vue
@@ -61,8 +61,8 @@ const date = computed(() => {
 <template>
   <div class="carte fr-mb-8w">
     <div class="section-title fr-mb-8w">
-      <h2>La situation de la sécheresse en France aujourd'hui</h2>
-      <span>{{ date }}</span>
+      <h2>La situation de la sécheresse en France</h2>
+      <span>Arrêtés du {{ date }}</span>
     </div>
     <div class="legende fr-mb-1w fr-grid-row fr-grid-row--center fr-grid-row--gutters">
       <div v-for="legend in legends" class="fr-grid-row fr-grid-row--center fr-col-xl-2 fr-col-12">
