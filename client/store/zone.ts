ed6d6b59c64a84c10cbe8249285c98ff75fa4920
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-07-20 10:16:13 +0200

    Utilisation de la route Zones pour les profils pro ✨

--- client/store/zone.ts
+++ client/store/zone.ts
@@ -7,27 +7,31 @@ export const useZoneStore = defineStore('zoneStore', () => {
   const zones: Ref<Zone[] | null> = ref(null);
   const zone: Ref<Zone | null> = computed(() => zones.value ? zones.value[0] : null);
 
-  function setZone(value: Zone, profile: string, departementConfig: Departement): void {
-    zones.value = [formatZone(value, profile, departementConfig)];
+  function setZones(value: Zone[], profile: string, departementConfig: Departement): void {
+    zones.value = formatZones(value, profile, departementConfig);
   }
 
   function resetZones(): void {
     zones.value = null;
   }
 
-  function formatZone(zone: Zone, profile: string, departementConfig: Departement): Zone {
-    if (!zone) {
-      return zone;
+  function formatZones(zones: Zone[], profile: string, departementConfig: Departement): Zone[] {
+    if (!zones || zones.length < 1) {
+      return zones;
     }
-    zone.profil = profile;
-    zone.usages = zone.usages?.sort((a, b) => a.usage.localeCompare(b.usage));
-    zone.departementConfig = departementConfig;
-    return zone;
+    zones.map(zone => {
+      zone.profil = profile;
+      zone.usages = zone.usages?.sort((a, b) => a.usage.localeCompare(b.usage));
+      zone.departementConfig = departementConfig;
+    })
+
+    zones.sort((a, b) => utils.getRestrictionRank(b) - utils.getRestrictionRank(a));
+    return zones;
   }
 
   function isParticulier(): boolean {
     return zone.value ? zone.value.profil === 'particulier' : false;
   }
 
-  return {setZone, resetZones, zones, zone, isParticulier}
+  return {setZones, resetZones, zones, zone, isParticulier}
 })
