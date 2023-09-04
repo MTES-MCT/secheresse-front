15364d2a8bbb9d0021e5c44e5c067f81f798f733
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-09-04 15:11:19 +0200

    Rajout de texte & full url social networks 🐛

---
+++ client/components/mixins/Share.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+import { Ref } from "vue";
+
+const props = defineProps<{
+  situationLabel: string
+  address: string
+}>();
+
+const socialMediaText: Ref<string> = computed(() => {
+  if (!props.situationLabel) {
+    return `Situation ${props.address}, pas de restrictions - ${useRuntimeConfig().public.appName}`;
+  } else {
+    return `Situation ${props.address}, en ${props.situationLabel}, des restrictions sont en cours - ${useRuntimeConfig().public.appName}`;
+  }
+});
+const urlToShare = encodeURIComponent(window.location.href);
+
+const networks = [
+  {
+    name: "facebook",
+    label: "Partager sur Facebook",
+    url: `https://www.facebook.com/sharer.php?u=${urlToShare}`
+  },
+  {
+    name: "twitter",
+    label: "Partager sur Twitter",
+    url: `https://twitter.com/intent/tweet?url=${urlToShare}&text=${socialMediaText.value}&hashtags=#VigiEau`
+  },
+  {
+    name: "linkedin",
+    label: "Partager sur LinkedIn",
+    url: `https://www.linkedin.com/sharing/share-offsite/?url=${urlToShare}`
+  }
+];
+</script>
+
+<template>
+  <DsfrShare title="Partagez votre situation - Attention l'adresse utilisée sera visible"
+             :networks="networks"/>
+</template>
